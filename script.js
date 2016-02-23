0b7a9a6debfeda30b36109a23641c8bcb039aa6b
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-02-22 19:32:27 -0500

    working on pushing to pickedCards array

--- script.js
+++ script.js
@@ -1,64 +1,47 @@
 var cards = [];
 var guesses = 0;
+var pickedCards = [];
 
-
-for (var i = 0; i < 25; i++) {
-  cards.push(i);
+for (var i = 1; i < 10; i++) {
+  cards.push(i, i);
 }
 
 for(var i = 0; i < cards.length; i++){
   document.getElementById('container').innerHTML += '<div class="square" id="square_'+i+'"></div>';
 }
 for(var i = 0; i < cards.length; i++){
-  document.getElementById('square_'+i).innerHTML += '<p>square </p>'+i;
+  document.getElementById('square_'+i).innerHTML += cards[i];
 }
-
-
 
 
 function revealSquare (){
-  var squares = document.querySelectorAll(".square");
+    var squares = document.querySelectorAll(".square");
+      for (var i = 0; i < squares.length; i++) {
+          squares[i].addEventListener('click', function(){
+                this.style.background = "white";
+                guesses += 1;
+                // pickedCards.push(this.innerHTML);
+
+                if (pickedCards.length === 0) {
+                  pickedCards.push(this.innerHTML);
+                }
+                else if (pickedCards.length === 1) {
+                  pickedCards.push(this.innerHTML);
+                  if (pickedCards[1] === pickedCards[2]) {
+                    pickedCards = [];
+                  }
+                  else {
+                    pickedCards = [];
+                  }
+                }
 
 
 
-    for (var i = 0; i < squares.length; i++) {
-        squares[i].addEventListener('click', function(){
-              this.style.background = "white";
-              guesses += 1;
-        });  // end eventListener function
+          });  // end eventListener function
 
-    } // end loop over all squares
-
-
-
-
-
-
-
+      } // end loop over all squares
 }   //end revealSquare function
 
 
-function changeColorBack() {
-  if (squares.style.background === "white") {
-    squares.style.background = "black";
-}
-}
 
-
-
-function runGame () {
-
-if (guesses < 3) {
-revealSquare();
-}
-else {
-changeColorBack()
-
-  }
-
-
-
-}
-
-
-runGame();
+  revealSquare();
